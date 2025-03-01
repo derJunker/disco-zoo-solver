1ebb323f7411425d9949cf44a7d993073a0b0655
Jonas Heitz <mail@jonas-heitz.eu>
2025-03-01 23:49:04 +0100

    Added java libs, vue

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,33 @@
 plugins {
     id("java")
+    id("org.springframework.boot") version "3.4.3"
+    id("io.spring.dependency-management") version "1.1.7"
 }
 
-group = "de.kvb.argus.cargo"
+group = "junker.disco.solver"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_23
+}
+
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    compileOnly("org.projectlombok:lombok")
+    annotationProcessor("org.projectlombok:lombok:1.18.30")
+
+    implementation("org.apache.logging.log4j:log4j-api:2.24.3")
+    implementation("org.apache.logging.log4j:log4j-core:2.24.3")
+    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.24.3")
 }
 
 tasks.test {
---
+++ src/main/java/junker/disco/zoo/solver/DiscoZooSolverSpringApplication.java
@@ -0,0 +1,11 @@
+package junker.disco.zoo.solver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoZooSolverSpringApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoZooSolverSpringApplication.class, args);
+    }
+}
