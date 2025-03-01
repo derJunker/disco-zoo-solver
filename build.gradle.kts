2b9107ba78d78b77820371258a981ce2c34a9f5e
Jonas Heitz <mail@jonas-heitz.eu>
2025-03-02 00:45:50 +0100

    basic server setup with tomcat done

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,12 @@ plugins {
     id("java")
     id("org.springframework.boot") version "3.4.3"
     id("io.spring.dependency-management") version "1.1.7"
+    war
 }
 
 group = "junker.disco.solver"
 version = "1.0-SNAPSHOT"
+
 
 repositories {
     mavenCentral()
@@ -28,6 +30,8 @@ dependencies {
     implementation("org.apache.logging.log4j:log4j-api:2.24.3")
     implementation("org.apache.logging.log4j:log4j-core:2.24.3")
     implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.24.3")
+
+    providedRuntime("org.springframework.boot:spring-boot-starter-tomcat")
 }
 
 tasks.test {
--- src/main/java/junker/disco/zoo/solver/DiscoZooSolverSpringApplication.java
+++ src/main/java/junker/disco/zoo/solver/DiscoZooSolverSpringApplication.java
@@ -2,9 +2,18 @@ package junker.disco.zoo.solver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DiscoZooSolverSpringApplication {
+public class DiscoZooSolverSpringApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(DiscoZooSolverSpringApplication.class);
+    }
+
+
     public static void main(String[] args) {
         SpringApplication.run(DiscoZooSolverSpringApplication.class, args);
     }
