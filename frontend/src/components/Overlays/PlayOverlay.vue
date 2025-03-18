804c2100957a8203bbf4722f40653e0b38265547
Jonas Heitz <mail@jonas-heitz.eu>
2025-03-19 00:35:58 +0100

    added new overlay for play modes

---
+++ frontend/src/components/Overlays/PlayOverlay.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="play-overlay">
+    <h3>Play</h3>
+    <div class="play-buttons">
+      <div class="btn btn-gradient color-action-neutral-2" @click="clickedReconstruct()">Reconstruct</div>
+      <div class="btn btn-gradient color-action-neutral-3" @click="clickedAccuracy()">Accuracy</div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.play-buttons {
+  display: flex;
+  flex-wrap: wrap;
+  column-gap: .5rem;
+  row-gap: .3rem;
+  justify-content: space-between;
+}
+.btn {
+  padding: .7rem 1rem;
+}
+</style>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+import router from "@/router";
+
+export default defineComponent({
+  name: 'play-overlay',
+  methods: {
+    clickedReconstruct() {
+      router.push({name: 'reconstruct'});
+    },
+    clickedAccuracy() {
+      router.push({name: 'accuracy'})
+    },
+  }
+})
+</script>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -3,6 +3,7 @@
     <div class="home-view-content">
       <h1>Disco Zoo Solver</h1>
       <menu-overlay v-if="showMenuOverlay" class="menu-overlay dock-bottom wood-menu"/>
+      <play-overlay v-if="showPlayOverlay" class="play-overlay dock-bottom wood-menu"/>
     </div>
     <menu-bar :on-first-button-click="onMenuClick" first-color-class="color-action-neutral-1" first-button-name="Menu"
               :on-second-button-click="onPlayClick" second-color-class="color-action-neutral-2"
@@ -22,13 +23,20 @@
   flex: 1;
   position: relative;
 }
-.menu-overlay {
+.menu-overlay, .play-overlay {
   position: absolute;
   bottom: 0;
   left: 0;
   right: 0;
-  max-width: min(70%, 400px);
   margin: auto;
+}
+
+.menu-overlay {
+  max-width: min(70%, 400px);
+}
+
+.play-overlay {
+  max-width: min(90%, 400px);
 }
 </style>
 
@@ -36,24 +44,30 @@
 import MenuBar from "@/components/MenuBar.vue";
 import {defineComponent} from "vue";
 import MenuOverlay from "@/components/Overlays/MenuOverlay.vue";
+import PlayOverlay from "@/components/Overlays/PlayOverlay.vue";
 
 export default defineComponent({
   name: 'HomeView',
-  components: {MenuOverlay, MenuBar},
+  components: {PlayOverlay, MenuOverlay, MenuBar},
 
   methods: {
     onMenuClick() {
+      if (!this.showMenuOverlay)
+        this.showPlayOverlay = false;
       this.showMenuOverlay = !this.showMenuOverlay;
     },
 
     onPlayClick() {
-      console.log('Play clicked');
+      if (!this.showPlayOverlay)
+        this.showMenuOverlay = false;
+      this.showPlayOverlay = !this.showPlayOverlay;
     }
   },
 
   data() {
     return {
-      showMenuOverlay: true
+      showMenuOverlay: false,
+      showPlayOverlay: true
     }
   }
 });
