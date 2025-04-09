164825d941adfe342ec17f416ee663a153ea20c1
Jonas Heitz <mail@jonas-heitz.eu>
2025-04-09 15:00:49 +0200

    sorted css into files

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,7 +4,10 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-import './assets/global.css'
+import './assets/css/transitions.css'
+import './assets/css/menus.css'
+import './assets/css/buttons.css'
+import './assets/css/global.css'
 
 const pinia = createPinia()
 const app = createApp(App);
